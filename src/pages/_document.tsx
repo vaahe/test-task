450e29df2a0c39969279d7d19b4bba977f5858b6
vaahe <vahebarseghyan9999@gmail.com>
2023-04-25 17:25:22 +0400

    remove addons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 import React from 'react';
 const Document = () => {
   return (
--- src/styles/Customers.ts
+++ src/styles/Customers.ts
@@ -1,4 +1,4 @@
 export const styles = {
-  container: 'w-full h-screen text-black-100 text-xl p-5 overflow-auto',
+  container: 'w-full h-screen text-black-100 text-xl p-5',
   header: 'font-bold text-[20px] text-default',
 };
